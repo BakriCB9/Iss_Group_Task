2bdfb8c46b6a3ef410cc2b45066c60b7e17c93e6
Mj1234ba <bakriaweja@gmail.com>
2025-07-30 03:35:33 +0300

    feat(splach_screen) implement splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,33 @@ import 'package:iss_task/core/di/service_locator.dart';
 import 'package:iss_task/core/routes/route_generator.dart';
 import 'package:iss_task/core/routes/routes.dart';
 import 'package:iss_task/core/theme/app_theme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   configureDependencies();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  initialization() async {
+    await Future.delayed(Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +39,7 @@ class MyApp extends StatelessWidget {
       title: 'Iss Group',
       theme: AppTheme.lightTheme,
       onGenerateRoute: RouteGenerator.getRoute,
-      initialRoute: Routes.home,
+      initialRoute: Routes.homeScreen,
     );
   }
 }
